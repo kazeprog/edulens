6d4ff1c057128257c7ba4b683259c9e52a2c037b
kazeprog <tozukakazetaka@gmail.com>
2025-12-25 06:54:49 +0900

    feat: Google AdSense審査用コードを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 
@@ -58,6 +59,13 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white text-slate-800`}
       >
+        {/* Google AdSense */}
+        <Script
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6321932201615449"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+          async
+        />
         <AuthProvider>
           {children}
         </AuthProvider>
